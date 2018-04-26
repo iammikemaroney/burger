cc06fe87c335c0285540b8b229ba651b9e6525c9
Mike Maroney <mikemaroney@customers-MacBook-Pro.local>
2018-04-25 23:41:49 -0400

    still buggy

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db",
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
